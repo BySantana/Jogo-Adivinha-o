18bebed5d0ef79dc0c2e26075c274879c15d64ed
BySantana <diegodrcg@gmail.com>
2022-04-12 14:41:25 -0300

    Commit 1

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Aula1_jogo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = new Random().Next(1, 5);
+
+            for (int i = 4; i > 0; i--)
+            {
+                Console.WriteLine("Você tem 5 chances para acertar o número de 1 a 5 que estou pensando");
+                int numero = int.Parse(Console.ReadLine());
+                if (numero == input)
+                {
+                    Console.WriteLine("Acertou!");
+                    i = 0;
+                }
+                else if (numero != input)
+                {
+                    Console.WriteLine($"Errou!");
+                    Console.WriteLine($"As chances estão acabando. Só restam {i}");
+                }
+               
+            }
+        }
+    }
+}
